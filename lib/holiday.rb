353d553c47aa8cc813e855f1e9fe434dd6060b48
Will Studabaker <will.studabaker@gmail.com>
2020-10-29 13:54:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,11 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
     puts "#{season.capitalize}:"
     holidays.each do |holiday, supplies|
-      puts "  #{holiday}: #{supplies.join(", ")}"
+      proper_name = holiday.to_s
+      proper_name = proper_name.split
+      proper_name = proper_name.capitalize!
+      proper_name = proper_name.join(" ")
+      puts "  #{proper_name}: #{supplies.join(", ")}"
     end
   end
 end
